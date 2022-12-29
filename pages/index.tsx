984db446221a1b659d6b25a93e3abfceaeacd0e2
Mark O'Connor <mark.oconnor14@gmail.com>
2022-12-29 23:49:01 +0700

    toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
+import { toast } from 'react-hot-toast'
 
 import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <main>
-      <Loader show />
-      <Link href="go!">Get er done!</Link>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
     </main>
   )
 }
