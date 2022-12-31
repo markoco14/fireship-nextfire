debb7d58c54b2e419532ec33fbc04c42fa567881
Mark O'Connor <mark.oconnor14@gmail.com>
2022-12-31 11:01:03 +0700

    google sign in

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,41 @@
-export default function EnterPage({ }) {
-    return (
-        <main>
-            <h1>Sign Up</h1>
-        </main>
-    )
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function EnterPage({}) {
+
+  const user = null;
+  const username = null;
+
+  return (
+    <main>
+      {user ?
+        !username ? <UsernameForm /> : <SignOutButton />
+        :
+        <SignInButton />
+      }
+    </main>
+  );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  )
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+
+function UsernameForm() {
+  return null
 }
