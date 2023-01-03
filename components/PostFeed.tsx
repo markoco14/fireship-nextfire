81454d6c32a2e0a152af68bd934b3de8fdda814e
Mark O'Connor <mark.oconnor14@gmail.com>
2023-01-04 00:10:45 +0800

    ssr user profile page

---
+++ components/PostFeed.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+export default function PostFeed({ posts, admin }) {
+    return posts?.map((post) => <PostItem post={post} key={post.slug} admin={admin}/>);
+}
+
+function PostItem({ post, admin = false }) {
+
+    const wordCount = post?.content.trim().split(/\s+/g).length;
+    const mintuesToRead = (wordCount / 100 + 1).toFixed(0);
+
+    return (
+        <div className="card">
+            <Link href={`/${post.username}`}>
+                By @{post.username}
+            </Link>
+            <Link href={`/${post.username}/${post.slug}`}>
+                <h2>
+                    {post.title}
+                </h2>
+            </Link>
+            <footer>
+                <span>
+                    {wordCount} words. {mintuesToRead} min read
+                </span>
+                <span> &#10084; {post.heartCount} Hearts</span>
+
+            </footer>
+        </div>
+    );
+}
