0bc7814176c4787d5d12821b62b29f8f769f4004
Mark O'Connor <mark.oconnor14@gmail.com>
2023-01-06 10:08:03 +0800

    metatags for seo

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -1,6 +1,9 @@
+import MetaTags from "../../components/Metatags"
+
 export default function AdminPostEdit({ }) {
     return (
         <main>
+            <MetaTags title="admin page" description="so good" image="null"/>
             <h1>You are slugging it!</h1>
         </main>
     )
