f2877e27e294d6d0c765bc0d8b054add0be48d2a
Mark O'Connor <mark.oconnor14@gmail.com>
2023-01-07 13:26:24 +0800

    fix real time hydration issues with post ids

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -4,49 +4,29 @@ import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 
 // tutorial version
-// export async function getStaticProps({ params }) {
-//   const userDoc = await getUserWithUsername(params.username);
-
-//   let post;
-//   let path;
-
-//   if (userDoc) {
-//     const postRef = userDoc.ref
-//     .collection('posts')
-//     .doc(params.slug)
-
-//     post = postToJSON(await postRef.get());
-
-//     path = postRef.path;
-//   }
-  
-//   return {
-//     props: { post, path },
-//     revalidate: 5000,
-//   };
-// }
-
-// my version
+// this version wasn't working at first
+// because I had saved the post documents with randomly generated ids for names
+// but the ids needed to be camel-case-versions of the article title, the same as the slug
 export async function getStaticProps({ params }) {
-    const userDoc = await getUserWithUsername(params.username);
+  const userDoc = await getUserWithUsername(params.username);
 
-    let posts;
-    let post;
+  let post;
+  let path;
 
-    if (userDoc) {
-      const postQuery = userDoc.ref
-      .collection('posts')
-      .where('slug', '==', params.slug)
+  if (userDoc) {
+    const postRef = userDoc.ref
+    .collection('posts')
+    .doc(params.slug)
 
-      posts = (await postQuery.get()).docs.map(postToJSON);
-      post = posts[0];
-      // console.log(post)
-    }
-    
-    return {
-      props: { post, path: 'posts/my-next-big-article' },
-      revalidate: 5000,
-    };
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+  
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
 }
 
 export async function getStaticPaths() {
@@ -72,25 +52,11 @@ export async function getStaticPaths() {
 
 
 export default function Post(props) {
-  // console.log(props.post.content)
-
-  // we only need to pass a path object
-  // if we want to get a real time post
-  // in case it has changed or something?
-  // so the path is allowing hydration - reacting to realtime data
-  // so when likes increase for example
-  // but also when the content is changed?
-  // const postRef = firestore.doc(props.path);
-  // const [realtimePost] = useDocumentData(postRef);
-  // const post = realtimePost || props.post;
-
-  // temporary fix to firestore problems
-  // no hyrdation
-  // probably have to re-render the page to get realtime hearts...
-  // lets see
-  // but I could also manage some state in the front-end to fake the hearts.
-  // the post content doesn't need to change in realtime
-  const post = props.post
+  // hydration, real time listening to the database
+  // updates heart count and content in real time
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+  const post = realtimePost || props.post;
   
   return (
     <main>
